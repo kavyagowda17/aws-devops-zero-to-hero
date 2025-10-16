6c75e3e9ad0db9ccbbf38e5e66220a794e37d19c
kavyagowda17 <72566823+kavyagowda17@users.noreply.github.com>
2025-10-16 11:33:37 -0400

    Replace container stop logic with greeting
    Removed container stopping logic and added greeting.

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,10 +2,4 @@
 set -e
 
 # Stop the running container (if any)
-container_id=`docker ps | awk -F " " '{print $1}'`
-if [ -n "$container_id" ]; then
-  echo "Stopping and removing container: $container_id"
-  docker rm -f "$container_id"
-else
-  echo "No running containers to stop."
-fi
+echo "Hi"
